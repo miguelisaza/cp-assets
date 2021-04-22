6e7ca706d7561b5f1229ae3eec6eee0a29e725ed
Miguel Isaza <misaji95@hotmail.com>
2021-04-22 16:05:42 -0500

    Add FileBox component

---
+++ src/components/FileBox/FileBox.js
@@ -0,0 +1,31 @@
+/* eslint-disable react/no-unused-state */
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import './FileBox.scss';
+
+class FileBox extends Component {
+	uploadFile = ({ target : { files } }) => {
+		const { onFileLoad } = this.props;
+		onFileLoad(Array.from(files));
+	}
+
+	render() {
+		return (
+			<div className="row justify-content-center text-center">
+				<form>
+					<div className="form-group files">
+						<label>Upload your assets</label>
+						<input onChange={this.uploadFile} type="file" className="form-control" multiple="" />
+					</div>
+				</form>
+			</div>
+		);
+	}
+}
+
+FileBox.propTypes = {
+	onFileLoad : PropTypes.func.isRequired,
+};
+
+export default FileBox;
